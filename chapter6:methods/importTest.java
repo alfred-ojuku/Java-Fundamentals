99bfb9aa71745a0bd9ca67255786c71d5305f514
Alfred Ojuku <alfred.okinyi@roam-electric.com>
2026-02-05 07:27:49 +0300

    upto passing arguments by values

---
+++ chapter6:methods/importTest.java
@@ -0,0 +1,9 @@
+public class importTest{
+	public static void main(String[] args){
+		int i = 70;
+		int j = 16;
+		int k = TestMax.max(i, j);
+
+		System.out.println("The maximum of " + i + " and " + j + " is " + k);
+	}
+}
