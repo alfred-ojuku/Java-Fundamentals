a0268e51225f2bbd39c31c3098f3550b8a66fce7
Alfred Ojuku <alfred.okinyi@roam-electric.com>
2026-02-12 08:31:14 +0300

    palindrom solution in a for loop

---
+++ chapter5:loops/PalindromeFor.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PalindromeFor{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+
+		// user input
+		System.out.print("Enter the english word: ");
+		String m = input.nextLine();
+
+		int len = m.length() - 1;
+		Boolean isPal = true;
+ 
+		for (int i = 0; i <= (m.length() - 1) / 2; i++){
+  			char x = Character.toLowerCase(m.charAt(i));
+  			char y = Character.toLowerCase(m.charAt(len - i));
+  			if (x != y){
+    				isPal = false;
+    				break;
+ 			 }
+		}
+
+		if (isPal){
+ 			System.out.println(m + " is a palindrome"); 
+		}
+		else{
+ 		 System.out.println(m + " is not a palindrome");
+		}	
+	}
+}
